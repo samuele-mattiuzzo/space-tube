4f55acc5000a9e3885ba9cdd2a94d6c2f16af9b2
Samuele Mattiuzzo <samumatt@gmail.com>
2016-03-21 15:42:48 +0000

    Better everything

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ var tag,
     form,
     video,
     viewCount,
-    spaceShip;
+    spaceShip, spaceShipSpeed,
+    statsDiv;
 
 tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
@@ -13,7 +14,8 @@ tag.src = "https://www.youtube.com/iframe_api";
 firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-form = document.forms[0]
+form = document.forms[0];
+statsDiv = document.getElementById('stats');
 
 form.addEventListener("submit", function(evt){
   evt.preventDefault();
@@ -21,15 +23,20 @@ form.addEventListener("submit", function(evt){
   video = form.elements['videoToExplore'].value;
   video = video.replace('https://www.youtube.com/watch?v=', '')
 
-  viewCount = form.elements['viewCounts'].value;
+  viewCount = form.elements['viewCount'].value;
+  viewCount = viewCount.replace(/,/g, '');
+  viewCount = parseInt(viewCount);
 
-  spaceShip = form.elements['spaceShip'].value;
+  spaceShip = form.elements['spaceShip'];
+  spaceShipSpeed = spaceShip.options[spaceShip.selectedIndex].value;
+  spaceShipSpeed = parseInt(spaceShipSpeed);
+  spaceShip = spaceShip.options[spaceShip.selectedIndex].text;
 
-  spawnPlayer(video, viewCount, spaceShip);
+  spawnPlayer(video, viewCount, spaceShip, spaceShipSpeed);
 });
 
 // all the methods!
-function spawnPlayer(videoId, viewCount, spaceShip) {
+function spawnPlayer(videoId, viewCount, spaceShip, spaceShipSpeed) {
   player = new YT.Player('player', {
     height: '390',
     width: '640',
@@ -42,6 +49,19 @@ function spawnPlayer(videoId, viewCount, spaceShip) {
 
 function onPlayerReady(event) {
     var duration = player.getDuration();
+    var timeSpent = viewCount * duration; // in seconds
+    timeSpent = timeSpent / 3600; // in hours
 
+    var distance = convertToDistance(timeSpent, spaceShipSpeed);
 
+    var message = "<p>";
+    message = message + "We could have traveled " + distance + " km in ";
+    message = message + timeSpent + " hours that we spent watching this";
+    message = message + "</p>";
+    statsDiv.innerHTML = message;
+
+}
+
+function convertToDistance(timeSpent, vehicleSpeed) {
+    return timeSpent * vehicleSpeed;
 }
